600f3f366fc970e60a5d81af8ec44ae640c847df
Paruthi, Gaurav <gparuthi@umich.edu>
2017-04-10 12:56:38 -0400

    To work with HEED framework.
    On init the config is set. If the app is not started, it means that phone was restarted and the app needs to be launched again.

--- src/android/MyService.java
+++ src/android/MyService.java
@@ -1,3 +1,6 @@
+// follow the instructions here : https://github.com/Red-Folder/bgs-core/wiki/Using-the-MyService-Sample
+// then copy this code into myService.java
+
 package com.red_folder.phonegap.plugin.backgroundservice.sample;
 
 import java.text.SimpleDateFormat;
@@ -7,6 +10,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.util.Log;
+import android.content.Intent;
+import android.content.ComponentName;
+import android.content.Context;
+
+import com.inteco.bleesmviewer.MainActivity;
 
 import com.red_folder.phonegap.plugin.backgroundservice.BackgroundService;
 
@@ -19,7 +27,22 @@ public class MyService extends BackgroundService {
 	@Override
 	protected JSONObject doWork() {
 		JSONObject result = new JSONObject();
+
 		
+			if (this.mHelloTo == "World"){
+				// app is not launched yet
+				Log.d(TAG, "App is not launched yet. Launching...");
+				Intent it = new Intent("android.intent.action.VIEW");
+				Context context     = getApplicationContext();
+				it.setComponent(new ComponentName(context.getPackageName(), MainActivity.class.getName()));
+				it.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				getApplicationContext().startActivity(it);
+			} else {
+				Log.d(TAG, "App is already launched.");
+			}
+		
+		
+
 		try {
 			SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss"); 
 			String now = df.format(new Date(System.currentTimeMillis())); 
