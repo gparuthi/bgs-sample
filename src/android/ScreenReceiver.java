05e6b4582f740b3c9a0ee45eba13cde20864e9eb
Paruthi, Gaurav <gparuthi@umich.edu>
2017-04-27 12:24:48 -0400

    updated

--- src/android/ScreenReceiver.java
+++ src/android/ScreenReceiver.java
@@ -3,11 +3,14 @@ package com.red_folder.phonegap.plugin.backgroundservice.sample;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.Context;
+import android.util.Log;
 
 
 public class ScreenReceiver extends BroadcastReceiver {
  
     private boolean screenOff;
+
+    private final static String TAG = MyService.class.getSimpleName();
  
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -16,7 +19,7 @@ public class ScreenReceiver extends BroadcastReceiver {
         } else if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
             screenOff = false;
         }
-        Log.d(TAG, "ScreenOff is "+ screenOff);
+        Log.d(TAG, "ScreenOff update. "+ screenOff);
         Intent i = new Intent(context, MyService.class);
         i.putExtra("screen_state", screenOff);
         context.startService(i);
